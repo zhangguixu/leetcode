f0e51d495cbf780e9beef3309b7c2d1515b7753c
gutzhang <gutzhang@tencent.com>
2019-10-28 19:45:23 +0800

    refactor: Remove Duplicates from Sorted Array

--- Array/Remove-Duplicates-from-Sorted-Array.go
+++ Array/Remove-Duplicates-from-Sorted-Array.go
@@ -2,41 +2,84 @@ package main
 
 import "fmt"
 
-func main() {
-	removeDuplicates([]int{1, 1, 2})
-	removeDuplicates([]int{0, 0, 1, 1, 1, 2, 2, 3, 3, 4})
-	removeDuplicates([]int{1, 1, 1}) // 边界
-	removeDuplicates([]int{1})       // 边界
-	removeDuplicates([]int{})        // 边界
+func print(nums []int, n int) {
+	for i := 0; i < n; i++ {
+		fmt.Printf(" %d ", nums[i])
+	}
+	fmt.Printf("\n")
 }
 
+func main() {
+	t1 := []int{1, 1, 2}
+	print(t1, removeDuplicates(t1))
+
+	t2 := []int{0, 0, 1, 1, 1, 2, 2, 3, 3, 4}
+	print(t2, removeDuplicates(t2))
+
+	t3 := []int{1, 1, 1}
+	print(t3, removeDuplicates(t3))
+
+	t4 := []int{1}
+	print(t4, removeDuplicates(t4))
+
+	t5 := []int{}
+	print(t5, removeDuplicates(t5))
+}
+
+/*
+	算法复杂度分析
+
+		Time complextiy : O(n)
+		Space complexity : O(1)
+
+	思路总结
+
+		此题目用到一些数组的通用思路：
+	
+		1）题目要求in-place，就说明操作只能是原数组的元素的交换，或者元素的覆盖
+		2）通过若干个额外的变量来记录一些状态（需要对题目进行分析）
+
+	solution是否已读
+
+		已读，solution和下面的实现是一致的。
+	
+	top 5 votes discuss 是否已读
+		
+		已读，吸收了top1的实现，见removeDuplicates_v1
+
+*/
 func removeDuplicates(nums []int) int {
-
-	var i, j int
-	total := len(nums)
-
-	if total <= 1 {
-		return total
+	if len(nums) == 0 {
+		return 0
 	}
-
-	for i < total-1 {
-		if nums[i] != nums[i+1] {
-			nums[j] = nums[i]
-			j++
+	idx := 1
+	for i := 1; i < len(nums); i++ {
+		if nums[i - 1] == nums[i] {
+			continue
 		}
-		i++
+		nums[idx] = nums[i]
+		idx++
 	}
+	return idx
+}
 
-	if j > 0 && nums[j-1] != nums[i] {
-		nums[j] = nums[i]
-		j++
+/*
+	votes最多的discuss给出的实现，实现更为简洁一点
+	
+	记录重复的个数
+	那么去重数组的下标就是 i - 重复的个数
+*/
+func removeDuplicates_v1(nums []int) int {
+	if len(nums) == 0 {
+		return 0
 	}
-
-	if j == 0 {
-		j = 1
+	count := 0
+	for i := 1; i < len(nums); i++ {
+		if nums[i] == nums[i - 1] {
+			count++
+			continue
+		}
+		nums[i - count] = nums[i]
 	}
-
-	fmt.Println(nums, j)
-
-	return j
+	return len(nums) - count
 }
--- README.md
+++ README.md
@@ -27,11 +27,11 @@
 
 ### Array
 
+- [Remove Duplicates from Sorted Array](https://leetcode.com/explore/featured/card/top-interview-questions-easy/92/array/727/)
 - [Rotate Array](https://leetcode.com/explore/featured/card/top-interview-questions-easy/92/array/646/)
 - [Move Zeroes](https://leetcode.com/problems/move-zeroes/) 难度：easy
 - [single number](https://leetcode.com/problems/single-number/) 难度：easy，考察点：位运算 异或
 - [two sum](https://leetcode.com/problems/two-sum/) 难度： easy，考察点：array hash table
-- [Remove Duplicates from Sorted Array](https://leetcode.com/explore/featured/card/top-interview-questions-easy/92/array/727/) 跟Move Zeroes的思路差不多
 - [Best Time to Buy and Sell Stock II](https://leetcode.com/explore/featured/card/top-interview-questions-easy/92/array/564/)
 - [Valid Sudoku](https://leetcode.com/explore/featured/card/top-interview-questions-easy/92/array/769/)
 - [Plus one](https://leetcode.com/explore/featured/card/top-interview-questions-easy/92/array/559/)
