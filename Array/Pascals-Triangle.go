75867c5e7ff050f0faba024584b99f44992167b4
gutzhang <gutzhang@tencent.com>
2019-11-07 10:04:58 +0800

    feat: Pascal's Triangle

---
+++ Array/Pascals-Triangle.go
@@ -0,0 +1,52 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(generate(2))
+}
+
+/*
+	杨辉三角计算
+	numRows = 5
+
+	[
+		[1],
+		[1,1],
+		[1,2,1],
+		[1,3,3,1],
+		[1,4,6,4,1]
+	]
+
+	规律是：
+
+	nums[i][j] = nums[i - 1][j - 1] + nums[i - 1][j]
+
+	若 i - 1 < 0， 则nums[i][j] = 1, 即nums[0][0] = 1
+	若 j - 1 < 0, nums[i][j] = nums[i - 1][j]
+	若 j < i，则 nums[i][j] = nums[i - 1][j - 1]
+
+	nums[0][0] = 1
+*/
+
+func generate(numRows int) [][]int {
+	nums := make([][]int, numRows, numRows)
+  for i := 0; i < numRows; i++ {
+		nums[i] = make([]int, i + 1, i + 1)
+		for j := 0; j <= i; j++ {
+			if i - 1 < 0 {
+				nums[i][j] = 1
+				break
+			}
+			if j - 1 >= 0 {
+				nums[i][j] += nums[i - 1][j - 1]
+			}
+			if j < i {
+				nums[i][j] += nums[i - 1][j]
+			}
+		}
+	}
+	return nums
+}
+
+
